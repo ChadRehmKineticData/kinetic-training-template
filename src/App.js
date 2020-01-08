675a6d371f3a63413b5c42a4818fbd3cb41b2670
ChadRehmKineticData <chad.rehm@kineticdata.com>
2020-01-08 09:55:29 -0600

    Set App to point to components dir

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./styles.css";
 import { Home } from "./components/home";
 
-export const App = () => <Home />;
+export const App = () => (
+  <Switch>
+    <Route path="/" component={Home} />
+  </Switch>
+);
